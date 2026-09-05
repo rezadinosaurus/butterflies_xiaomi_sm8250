90d2654a83a59098f23c0d3fce6752b4d916e8e9
starscroch <starscroch90@gmail.com>
2026-09-05 09:35:03 +0700

    drivers: power: ds28e16: fix bad_udelay

--- drivers/power/supply/maxim/ds28e16.c
+++ drivers/power/supply/maxim/ds28e16.c
@@ -343,7 +343,7 @@ int DS28E16_standard_cmd_flow(unsigned char *write_buf, int delay_ms,
 	// check for strong pull-up
 	if (delay_ms > 0) {
 		write_byte(CMD_RELEASE_BYTE);
-		Delay_us(1000 * delay_ms);
+		usleep_range(1000 * delay_ms, 1200 * delay_ms);
 	}
 
 	read_byte();
